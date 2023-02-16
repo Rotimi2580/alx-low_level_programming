565e2c00b5d0f724c411e18ddf9869df2ed4d015
Omitogun R. Olukunke <rotimi.omitoogun@gmail.com>
2023-02-16 13:52:48 -0800

    First C file using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point of the program
+ *
+ * Decription: prints or displays "Programmming is like building a multilingual puzzle"
+ *
+ * Return: Always 0 iif no error in the running of the program
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingualpuzzle\n");
+	return (0);
+}
