a920f6c614005c4aec7ba8d09205cd3d448c9511
Omitogun R. Olukunke <rotimi.omitoogun@gmail.com>
2023-02-21 14:49:59 -0800

    Commit on small letters

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - Entry points
+ *
+ * Return: Always 0 for success.
+ */
+
+void print_alphabet(void)
+{
+	char alp;
+	for (alp = 'a'; alp <= 'z'; alp++)
+		_putchar(alp);
+	_putchar('\n');
+}
