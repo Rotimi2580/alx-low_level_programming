24d1d147e7fd07b929bad47998359ca2edfa915b
Omitogun R. Olukunke <rotimi.omitoogun@gmail.com>
2023-02-27 08:28:08 -0800

    Rev string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,19 +1,29 @@
-#include "main.h"
+#include "holberton.h"
+
 /**
- * rev_string - reverses a string
- * @s: pointer to a char
- * Return: Always 0
+ * rev_string - reverse string
+ * @s: string
+ * Return: void
  */
+
 void rev_string(char *s)
 {
-	int len = strlen(s);
-	int i;
-	char tmp;
+	int i, max, half;
+	char first, last;
 
-	for (i = 0; i < len / 2; i++)
+	i = 0;
+	while (s[i] != '\0')
 	{
-		tmp = s[i];
-		s[i] = s[len - i - 1];
-		s[len - i - 1] = tmp;
+		i++;
+	}
+	max = i - 1;
+	half = max / 2;
+	while (half >= 0)
+	{
+		first = s[max - half];
+		last = s[half];
+		s[half] = first;
+		s[max - half] = last;
+		half--;
 	}
 }
