187a25eb66596eb11b8e051f5d6a107993e0353e
Rotimi2580 <rotimi.omitoogun@gmail.com.com>
2023-10-02 14:55:37 -0700

    fixed tasks 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,59 +1,47 @@
 #include "main.h"
 
 /**
- * _strlen - returns length of string
- * @s: string
- * Return: length of string
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
+ *
+ * Return: 1 if it success. -1 if it fails.
  */
-
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (s[len] != '\0')
-	{
-		len++;
-	}
-
-	return (len);
-}
-
-
-/**
- * append_text_to_file - appends a text at the end of a file
- * @filename: name of file
- * @text_content: text
- * Return: 1 - success, -1 error
- */
-
-int append_text_to_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	unsigned int length;
-	ssize_t bytes_written;
+	int nletters;
+	int rwr;
 
-	if (filename == NULL)
+	if (!filename)
 	{
 		return (-1);
 	}
 
-	if (text_content == NULL)
-	{
-		return (1);
-	}
-	fd = open(filename, O_WRONLY | O_APPEND);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 	{
 		return (-1);
 	}
-	length = _strlen(text_content);
 
-	bytes_written = write(fd, text_content, length);
-	if (bytes_written == -1)
+	if (!text_content)
+	{
+		text_content = "";
+	}
+
+	for (nletters = 0; text_content[nletters]; nletters++)
+	{
+		;
+	}
+
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
 	{
 		return (-1);
 	}
+
 	close(fd);	/*close the file*/
 
 	return (1);
